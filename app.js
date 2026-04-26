f65d205a1edfdca7320a5978a897b00694e94124
stephen muiru <stevembugua26@gmail.com>
2026-04-26 16:58:40 +0300

    worked on the router and form

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (res, req) =>{
-  res.render('index')
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-app.get('/new', (res, req) => {
-    res.render('new')
+app.get('/new', (req, res) => {
+  res.render('form');
 });
+
+app.post('/new', (req, res) =>{
+
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect('/')
+})
 
 app.listen(3000, () => {
-    console.log("server running at PORT 3000")
-})
+  console.log("server running at PORT 3000");
+});
